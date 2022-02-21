5a6e2305c7267155211c41f53a5301b65a6889d9
Noah Love <noah.love@columbia.edu>
2022-02-21 05:04:25 +0000

    Removed unused packages

--- app/global.R
+++ app/global.R
@@ -62,8 +62,8 @@ library(readxl)
 
 
 #Data Processing
-rstrt_insp <- read_excel("data/New_York_City_Restaurant_Inspection_Results.xlsx")
-rs_open <- read.csv("data/Open_Restaurant_Applications.csv")
+rstrt_insp <- read_excel("../data/New_York_City_Restaurant_Inspection_Results.xlsx")
+rs_open <- read.csv("../data/Open_Restaurant_Applications.csv")
 
 
 
