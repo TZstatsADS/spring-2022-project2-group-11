ede32f0e8446ebdbde53cd39501ed2046dc1a53c
Noah Love <noah.love@columbia.edu>
2022-02-21 05:08:55 +0000

    Moved global file to docs

---
+++ doc/global.R
@@ -0,0 +1,6 @@
+library(readxl)
+
+#Data Processing
+rstrt_insp <- read_excel("data/New_York_City_Restaurant_Inspection_Results.xlsx")
+rs_open <- read.csv("data/Open_Restaurant_Applications.csv")
+
