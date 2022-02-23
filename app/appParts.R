c36e9e411fa065fe601311bb8ce56593d7b1fb0c
Noah Love <noah.love@columbia.edu>
2022-02-23 17:39:08 -0500

    Added proper dates

--- app/appParts.R
+++ app/appParts.R
@@ -252,7 +252,12 @@ astheticPage <- function() {
                                 "2011",
                                 "2012",
                                 "2013",
-                                "2014")),
+                                "2014",
+                                "2015",
+                                "2016",
+                                "2017",
+                                "2018",
+                                "2019")),
         radioButtons(inputId = "sex",
                      label = "Sex:",
                      choices = c(
